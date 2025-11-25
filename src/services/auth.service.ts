9f8786c031cfde4591432efec112aa7096cf0589
iya-campo <ilyannacampo@gmail.com>
2025-11-26 03:42:57 +0800

    Fix: Modified service imports

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,4 @@
-import prisma from "../config/db";
+import prisma from "config/db";
 import bcrypt from 'bcrypt';
 import * as crypto from 'crypto';
 import { signToken } from '../utils/jwtUtils';
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,4 @@
-import prisma from '../config/db';
+import prisma from 'config/db';
 import { User } from '@prisma/client';
 import { hashPassword } from "../utils/passwordUtils";
 
